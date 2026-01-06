214db8ed555717fca5115b433c17fb2bfc427f72
Caio Tarifa <caiotarifa@gmail.com>
2026-01-06 12:47:55 -0300

    feat(auth): create AuthCard component for authentication UI

---
+++ app/components/auth/AuthCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <UCard
+    class="backdrop-blur bg-white/25 dark:bg-white/2"
+    variant="soft"
+  >
+    <slot />
+
+    <template
+      v-if="$slots.footer"
+      #footer
+    >
+      <slot name="footer" />
+    </template>
+  </UCard>
+</template>
